081c1c127ce82831edcab2b90ab50afd5689cc19
pudep <261843105+pudep@users.noreply.github.com>
2026-07-09 11:51:20 +0530

    updated cargo vars

--- fish/conf.d/00-env.fish
+++ fish/conf.d/00-env.fish
@@ -5,6 +5,11 @@ fish_add_path $HOME/.cargo/bin
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx LESS '-R -F -X -i -M -W -q'
-set -gx CARGO_TARGET_DIR "$HOME/.cargo_targets"
+
+# conf.d/00-env.fish
+set -gx CARGO_HOME "$HOME/.CARGO_HOME"
+set -gx RUSTUP_HOME "$HOME/.RUSTUP_HOME"
+set -gx CARGO_TARGET_DIR "$HOME/.CARGO_TARGET_DIR"
 set -gx RUSTC_WRAPPER sccache
+fish_add_path "$CARGO_HOME/bin"
 set -gx TMPDIR /data/data/com.termux/files/usr/tmp
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,1 @@
 set -g fish_greeting ""
-set -gx PATH "$HOME/.cargo/bin" $PATH
-set -gx PATH $HOME/.cargo/bin $PATH
